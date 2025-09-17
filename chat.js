15c7c83aed15a77996232dbcc3cf5da615f55def
netherhome <rhyslbeale@gmail.com>
2025-09-17 11:58:47 +1000

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api-inference.huggingface.co/models/gpt2", {
     method: "POST",
     headers: {
-      "Authorization": "Bearer YOUR_HF_TOKEN",
+      "Authorization": "Bearer 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",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ inputs: prompt })
